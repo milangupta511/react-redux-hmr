bbdb1e3d971f2dcb89a6d51d935454db1dd7492d
Milan Gupta <mgupta64@sapient.com>
2017-05-14 00:17:50 +0530

    Initializes starter kit for react,redux with hmr

---
+++ tools/startMessage.js
@@ -0,0 +1,6 @@
+import colors from 'colors';
+//will run first as its written in prestart, which runs before start when you do npm start(similaryly poststart)
+//esw - eslint watcher
+/* eslint-disable no-console */
+
+console.log('Starting app in dev mode...'.green);
---
+++ webpack.config.dev.js
@@ -0,0 +1,36 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  debug: true, //displays debug info
+  devtool: 'cheap-module-source-map',
+  noInfo: false, //displays all the file is bundled. adds lots of noise
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+    path.resolve(__dirname, 'src/index') //acutal entry point is always passed last
+  ],
+  target: 'web', //can also be set as node, so that node can work on it. using web, it bundles in such a way that web browser can understand it
+  output: {
+    path: __dirname + '/dist', //Creates bundle in memory and provide it to browser Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: { //tells webpack dev server where our code is
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), //enables us to replace modules without doing full page refresh
+    new webpack.NoErrorsPlugin() //keep errors from breaking the hot module replacement experience. It gives nice error messages
+  ],
+  module: {
+    loaders: [ //recommended settings for bootstrap
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
